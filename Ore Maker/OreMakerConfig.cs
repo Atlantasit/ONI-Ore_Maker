1ce4692f70267b0f71961de53ea2ea7accef7f7a
Atlantasit <marx.leonard@gmx.de>
2023-08-24 21:10:19 +0200

    more configs

--- Ore Maker/OreMakerConfig.cs
+++ Ore Maker/OreMakerConfig.cs
@@ -5,7 +5,6 @@ namespace OreMakerConfig
 {
     public class OreMakerConfig : IBuildingConfig
     {
-        private readonly ConduitPortInfo conduitPort = new ConduitPortInfo(ConduitType.Gas, new CellOffset(-1, 2));
         public const float OreStorage_Capacity = 550f;
         public const float Emit_Mass = 500f;
         public const float WaterStorage_Capacity = 250f;
@@ -20,10 +19,31 @@ namespace OreMakerConfig
         {
             int width = 4;
             int height = 3;
+            string anim = "oremaker_kanim";
             int hitpoints = 25;
             float construction_time = 30f;
             float[] tieR3_1 = BUILDING:CONSTRUCTION_MASS_KG.TIER3;
             string[] allMetals = MATERIALS.ALL_METALS;
+            float melting_point = 800f;
+            BuildLocationRule buidingLocationRule = BuildLocationRule.OnFloor;
+            EffectorValues tieR3_2 = NOISE_POLLUTION.NOISY.TIER2;
+            EffectorValues tieR3_3 = BUILDINGS.DECOR.PENALTY.TIER3;
+            BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef("OreMaker", width, height, anim, hitpoints, construction_time, tieR3_1, allMetals, melting_point, buidingLocationRule, tieR3_3, tieR3_2);
+            buildingDef.RequiresPowerInput = true;
+            buildingDef.PowerInputOffset = new CellOffset(0, 0);
+            buildingDef.EnergyConsumptionWhenActive = 480f;
+            buildingDef.InputConduitType = ConduitType.Gas;
+            buildingDef.UtilityInputOffset = new CellOffset(-1, 2);
+            buildingDef.InputConduitType = ConduitType.Liquid;
+            buildingDef.UtilityInputOffset = new CellOffset(-1, 0);
+            buildingDef.AudioCategory = "HollowMetal";
+            buildingDef.ViewMode = OverlayModes.TileMode.ID;
+            buildingDef.ModifiesTemperature = false;
+        }
+
+        public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag)
+        {
+            base.ConfigureBuildingTemplate(go, prefab_tag);
         }
     }
 }
